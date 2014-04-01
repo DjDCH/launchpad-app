0733fe1a6b3b712d962f58884fb0a8d03942e5bb
DjDCH <coding@djdch.com>
2014-03-31 21:19:05 -0400

    LaunchpadApp is now invoked with a launchpad instance.

--- launchpad-app.js
+++ launchpad-app.js
@@ -1,24 +1,21 @@
-var Launchpad = require('./lib/launchpad.js');
-
-var LaunchpadApp = function (listeners) {
-  var pad = new Launchpad();
-
+var LaunchpadApp = function (launchpad, listeners) {
   for (ev in listeners)
-    pad.on(ev, listeners[ev].bind(pad));
+    launchpad.on(ev, listeners[ev].bind(launchpad));
 
-  process.on('SIGINT', pad.stop.bind(pad));
-  pad.midi.get.on('message', function (delta, msg) {
+  process.on('SIGINT', launchpad.stop.bind(launchpad));
+  launchpad.midi.get.on('message', function (delta, msg) {
     var pos = (msg[0] == 144)
             ? [msg[1] % 16, 8 + ~(msg[1] / 16)]
             : [msg[1] - 104, 8];
-    pad.emit('key', (msg[2] == 127), pos);
+    launchpad.emit('key', (msg[2] == 127), pos);
   });
 
-  pad.emit('start');
-  return pad;
+  launchpad.emit('start');
+  return launchpad;
 }
 
 LaunchpadApp.colors = require('./lib/colors.js');
 LaunchpadApp.buttons = require('./lib/buttons.js');
+LaunchpadApp.Launchpad = require('./lib/launchpad.js');
 
 module.exports = LaunchpadApp;
